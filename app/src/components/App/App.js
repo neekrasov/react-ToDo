5c0a2472b07be55c359834e85f047865eea63792
neekrasov <neekrasov@gmail.com>
2022-08-15 12:34:03 +0300

    fix error due to property key

--- app/src/components/App/App.js
+++ app/src/components/App/App.js
@@ -8,10 +8,10 @@ import './App.sass';
 const App = () => {
 
     const data = [
-        {name: "Alex", salary: 10, increase: true},
-        {name: "John", salary: 100, increase: false},
-        {name: "Nika", salary: 1000, increase: true},
-        {name: "Vasya", salary: 1000, increase: false},
+        {name: "Alex", salary: 10, increase: true, key: 1},
+        {name: "John", salary: 100, increase: false, key: 2},
+        {name: "Nika", salary: 1000, increase: true, key: 3},
+        {name: "Vasya", salary: 1000, increase: false, key: 4},
     ]
 
     return (
